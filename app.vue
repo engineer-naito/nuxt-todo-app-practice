24c6dca236ef55a1a6b3129678083bd097bb7561
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-11-22 15:12:38 +0000

    use UnoCSS Attributify mode

--- app.vue
+++ app.vue
@@ -3,7 +3,15 @@ const count = ref("0");
 </script>
 
 <template>
-  <div class="flex flex-column items-center justify-center h-screen text-4xl font-bold text-green-600">
+  <div
+    flex="~ col"
+    items-center
+    justify-center
+    h-screen
+    text-4xl
+    font-bold
+    text-green-600
+  >
     <div>Hello World!</div>
     <div>{{ count }}</div>
   </div>
