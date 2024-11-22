f0977512ce70936ec1701079c6f0ef7b42bd16d2
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-11-22 00:52:45 +0000

    setup unocss

--- app.vue
+++ app.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <div class="flex items-center justify-center h-screen text-4xl font-bold text-green-600">
+    Hello World!
   </div>
 </template>
