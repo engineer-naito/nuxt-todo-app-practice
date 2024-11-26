2ad5e436d142cf589ea74e5722702209cb9037dc
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-11-26 15:06:53 +0000

    tell eslint defineNuxtConfig exists

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,4 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
-import { defineNuxtConfig } from "nuxt/config";
-
 export default defineNuxtConfig({
   modules: ["@nuxt/eslint"],
   devtools: { enabled: true },
