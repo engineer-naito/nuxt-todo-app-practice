515c879805478e3bf05f88af70b8b5093a1e9230
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-11-27 14:53:14 +0000

    add unocss/reset

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint", "@prisma/nuxt", "@nuxtjs/supabase"],
+  modules: [
+    "@nuxt/eslint",
+    "@prisma/nuxt",
+    "@nuxtjs/supabase",
+  ],
   devtools: { enabled: true },
-
+  css: [
+    "@unocss/reset/tailwind.css",
+  ],
   future: {
     compatibilityVersion: 4,
   },
