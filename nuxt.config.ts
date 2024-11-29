5a06f58a7177f467033cd9bd90c3820d0a508ef5
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-11-29 15:29:18 +0000

    setup eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
-})
+  modules: ["@nuxt/eslint"],
+  devtools: { enabled: true },
+  compatibilityDate: "2024-11-01",
+  eslint: {
+    config: {
+      stylistic: {
+        indent: 2,
+        quotes: "double",
+        semi: true,
+      },
+    },
+  },
+
+});
