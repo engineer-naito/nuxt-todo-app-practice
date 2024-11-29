14f2a62d9208c482d34d7f17c2a2f3e0d437cfa7
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-11-29 15:32:56 +0000

    compatible with ver.4

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,6 +2,9 @@
 export default defineNuxtConfig({
   modules: ["@nuxt/eslint"],
   devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
   compatibilityDate: "2024-11-01",
   eslint: {
     config: {
