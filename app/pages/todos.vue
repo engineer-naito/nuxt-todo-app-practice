a17ad48317933baedf7081b5721718dcb795741a
kosuke222naito <kosuke.naito.engineer@gmail.com>
2024-11-30 06:09:01 +0000

    finally succeeded fetch from db with prisma 😭

--- app/pages/todos.vue
+++ app/pages/todos.vue
@@ -1,14 +1,21 @@
 <script setup lang="ts">
 const todos = ["eat", "sleep", "rave"];
+
+const { data: userProfile } = await useFetch("/api/userProfile", {
+  method: "GET",
+});
 </script>
 
 <template>
-  <ul>
-    <li
-      v-for="todo in todos"
-      :key="todo"
-    >
-      {{ todo }}
-    </li>
-  </ul>
+  <div>
+    <h1>{{ userProfile?.displayName }}'s Todo</h1>
+    <ul>
+      <li
+        v-for="todo in todos"
+        :key="todo"
+      >
+        {{ todo }}
+      </li>
+    </ul>
+  </div>
 </template>
---
+++ server/api/userProfile/index.get.ts
@@ -0,0 +1,26 @@
+import prisma from "@@/lib/prisma";
+import { serverSupabaseUser } from "#supabase/server";
+
+export default defineEventHandler(async (event) => {
+  const user = await serverSupabaseUser(event);
+
+  if (!user) {
+    throw createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized",
+    });
+  }
+
+  const profile = await prisma.userProfile.findUnique({
+    where: { userId: user.id },
+  });
+
+  if (!profile) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Profile not found",
+    });
+  }
+
+  return profile;
+});
